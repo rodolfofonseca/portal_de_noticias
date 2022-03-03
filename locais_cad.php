de550edaacb5ce0cbecc5c07c903c2a9a667c9f0
Rodolfo Fonseca <rodolfofonseca01@gmail.com>
2022-03-02 22:48:06 -0300

    criação dos locais

---
+++ locais_cad.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'includes/headerUsuario.php';
+require_once 'controller/modelos/Locais.php';
+require_once 'controller/LocaisCtr.php';
+?>
+<div class="container-fluid py-3">
+    <div class="container">
+        <div class="bg-light py-2 px-4 mb-3">
+            
+        </div>
+    </div>
+</div>
+<?php
+require_once 'includes/footerUsuario.php';
+?>
