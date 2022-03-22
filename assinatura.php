bf9555e46860066ef90be1349ab3811ead02e125
Rodolfo Fonseca <rodolfofonseca01@gmail.com>
2022-03-22 06:43:19 -0300

    finalização assinatura. Agora falta pouco

--- assinatura.php
+++ assinatura.php
@@ -4,21 +4,27 @@ require_once 'controller/UsuarioCtr.php';
 require_once 'controller/modelos/Usuario.php';
 $email = (string) '';
 $retorno = (bool) false;
+$existe = (bool) false;
 if (isset($_GET['email']) == true) {
     $email = $_GET['email'];
     $logDoSistema = new LogDoSistema();
     $usuario = new UsuarioCtr();
     $model = new Usuario();
-    $model->setEmailUsuario($email);
-    $model->setStatusUsuario('A');
-    $retorno = $usuario->SalvarContato($model);
+    $retorno_email = $usuario->Pesquisar("select * from contato where email_contato = '".$model->getEmailUsuario()."';");
+    if(empty($retorno_email) == false){
+        $model->setEmailUsuario($email);
+        $model->setStatusUsuario('A');
+        $retorno = $usuario->SalvarContato($model);
+    }else{
+        $retorno = false;
+        $existe = true;
+    }
 } else {
     $retorno = false;
 }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <title>ASSPOL - Associação Polícial</title>
@@ -32,13 +38,13 @@ if (isset($_GET['email']) == true) {
 
 <body>
     <?php
-    if ($retorno == true) {
+    if ($retorno == true && $existe = false) {
     ?>
         <script>
             let timerInterval
             Swal.fire({
-                title: 'Auto close alert!',
-                html: 'I will close in <b></b> milliseconds.',
+                title: 'Sucesso!',
+                html: 'Seja bem vindo a nossa lista, em breve você receberá <br/> as melhores notícias para ficar bem informado',
                 timer: 2000,
                 timerProgressBar: true,
                 didOpen: () => {
@@ -52,20 +58,19 @@ if (isset($_GET['email']) == true) {
                     clearInterval(timerInterval)
                 }
             }).then((result) => {
-                /* Read more about handling dismissals below */
                 if (result.dismiss === Swal.DismissReason.timer) {
                     window.open('index.php');
                 }
-            })
+            });
         </script>
     <?php
-    } else {
+    } else if($retorno == false && $existe == false){
     ?>
         <script>
             let timerInterval
             Swal.fire({
                 title: 'erro!',
-                html: 'I will close in <b></b> milliseconds.',
+                html: 'Aconteceu um erro inesperado durante o processo de cadastro.<br/> por favor tente novamente mais tarde',
                 timer: 2000,
                 timerProgressBar: true,
                 didOpen: () => {
@@ -79,13 +84,38 @@ if (isset($_GET['email']) == true) {
                     clearInterval(timerInterval)
                 }
             }).then((result) => {
-                /* Read more about handling dismissals below */
                 if (result.dismiss === Swal.DismissReason.timer) {
                     window.open('index.php');
                 }
-            })
+            });
         </script>
     <?php
+    }else if($retorno == false && $existe == true){
+        ?>
+        <script>
+            let timerInterval
+            Swal.fire({
+                title: 'erro!',
+                html: 'O email que está tentando realizar o cadastro já se encontra em nossa base de dados.',
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                        b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                }
+            }).then((result) => {
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    window.open('index.php');
+                }
+            });
+        </script>
+        <?php
     }
     ?>
 </body>
